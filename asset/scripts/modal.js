119c34c34a7214a8bc9271dec939e86ca5976f1c
MINWOO CHA <chamw53@naver.com>
2023-11-14 18:43:11 +0900

    모달페이지 모바일

---
+++ asset/scripts/modal.js
@@ -0,0 +1,29 @@
+'use strict'
+
+let dimBg = document.createElement('div');
+let wrap = document.querySelector('.wrap');
+dimBg.setAttribute('class', 'dim');
+
+class Modal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    open() {
+        let md = document.querySelector(`${this.name}`);
+        let okBtn = md.querySelector('.ok');
+        
+        md.classList.add('on', `${this.type}`);
+        wrap.appendChild(dimBg);
+
+        okBtn.addEventListener('click', function () {
+            md.classList.remove('on');
+            dimBg.remove();
+        })
+    }
+    close() {
+        let md = document.querySelector(`${this.name}`);
+        md.classList.remove('on');
+        dimBg.remove();
+    }
+}
