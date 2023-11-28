18e6a2f91671ea162b3b83273e2f1c34b52503cd
MINWOO CHA <chamw53@naver.com>
2023-11-28 15:52:09 +0900

    스티키 xscroll 추가

--- asset/scripts/sticky.js
+++ asset/scripts/sticky.js
@@ -7,4 +7,24 @@ stickyTab.forEach((el, index) => {
     if (index == 1) el.style.top = stickyTab[index - 1].offsetHeight + 'px'
     if (index == 2) el.style.top = stickyTab[index - 1].offsetHeight + stickyTab[index - 2].offsetHeight + 'px'
 })
-// reduce 로 누적높이갑 더해서 붙이고 싶은데 방법 찾는중..
+// reduce 로 누적높이값 더해서 붙이고 싶은데 방법 찾는중..
+
+let scrollWrap = document.querySelector('.x-scroll ul');
+let scrollList = document.querySelectorAll('.x-scroll li');
+
+scrollList.forEach((li, index) => {
+    li.addEventListener('click', () => {
+        siblingsRemove(li, 'on')
+        li.classList.add('on');
+        scrollWrap.scrollTo({ left: (li.offsetWidth * (index)), behavior: 'smooth' });
+        console.log(li.offsetWidth * index);
+    })
+})
+
+function siblingsRemove(t, removeClass) {
+    const children = t.parentElement.children;
+
+    for (let i = 0; i < children.length; i++) {
+        children[i].classList.remove(removeClass);
+    }
+}
