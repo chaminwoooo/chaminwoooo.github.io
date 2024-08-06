e1213e5d116eac74a66187651655b93f2c551414
MINWOO CHA <chamw53@naver.com>
2024-08-06 21:45:49 +0900

    경력 WIP

--- asset/scripts/main.js
+++ asset/scripts/main.js
@@ -3,15 +3,15 @@
 
 window.onload = function () {
 
-    setTimeout(() => {
-        TypeHangul.type('.logo',{
-            intervalType : 100,
-        })
+    // setTimeout(() => {
+    //     TypeHangul.type('.logo',{
+    //         intervalType : 100,
+    //     })
         
-        let $target = document.querySelector('.logo');
-        $target.addEventListener('th.endType', function (e) {
-            this.classList.add('done');
-        });    
-    }, 2000);
+    //     let $target = document.querySelector('.logo');
+    //     $target.addEventListener('th.endType', function (e) {
+    //         this.classList.add('done');
+    //     });    
+    // }, 2000);
 }
 
