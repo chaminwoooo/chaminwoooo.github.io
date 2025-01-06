9a0c3b57f0dc7d5eb61ee22a9e1044414d9bbdda
MINWOO CHA <chamw53@naver.com>
2025-01-06 22:32:00 +0900

    wip

---
+++ asset/scripts/modal.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function mdOpen(mdName) {
+    document.querySelector(mdName).showModal();
+}
+
+function mdClose(mdName) {
+    document.querySelector(mdName).close();
+}
+
+function toastOpen(toastName) {
+    let toast = document.querySelector(toastName);
+    
+}
