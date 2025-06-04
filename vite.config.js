256bba29a4479063ae34f16c094709f3c811b96e
MINWOO CHA <chamw53@naver.com>
2025-06-04 22:24:08 +0900

    0604

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "src/scss/abstracts/variables" as *;`
+        additionalData: `@use "src/scss/abstracts/index" as *;`
       }
     }
+    
+  },
+  base: '/', // 루트 도메인 배포용
+  build: {
+    outDir: 'dist'
   }
 });
